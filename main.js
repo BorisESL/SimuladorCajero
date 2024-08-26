56bc744bdf1083257108d88b3e9f6415edd85b3f
boris <boris.salgadolorca@gmail.com>
2024-08-25 20:53:47 -0400

    logica

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ function validarPin() {
 }
 
 function consultarSaldo() {
-    alert ("Su saldo actual es $ " + saldo);
+    alert("Su saldo actual es $ " + saldo);
 }
 
 function retirarDinero() {
     let monto = parseFloat(prompt("Ingrese monto a retirar"));
-    if (monto <= 0 || (isNaN(monto) )) {
-       console.log("Monto inválido. Favor ingrese un monto válido")
-    } else if (monto <= 0) { 
-        console.log("Saldo insuficiente")
+    if (monto <= 0 || (isNaN(monto))) {
+        alert("Monto inválido. Favor ingrese un monto válido")
+    } else if (monto > saldo) {
+        alert("Saldo insuficiente")
     } else {
         saldo -= monto;
-        console.log("Retiro exitoso. Su saldo actual es de :" + saldo);
+        alert("Retiro exitoso. Su saldo actual es de :" + saldo);
     }
 }
 
@@ -37,10 +37,36 @@ function depositarDinero() {
         console.log("monto inválido. Favor ingrese un monto valido");
     } else {
         saldo += monto;
-        console.log("Depósito exitoso. Su saldo actual es de : " + saldo);
+        alert("Depósito exitoso. Su saldo actual es de : " + saldo);
     }
 }
 
-function cajeroAutomatico () {
-    
+function cajeroAutomatico() {
+    if (validarPin()) {
+        for (let continuar = true; continuar;) {
+            let opcion = prompt("Seleccione una opcion:\n1. consultar saldo.\n2. retirar dinero.\n3.depositar dinero.\n4. salir ");
+
+            switch (opcion) {
+                case "1":
+                    consultarSaldo();
+                    break;
+                case "2":
+                    retirarDinero();
+                    break;
+                case "3":
+                    depositarDinero();
+                    break;
+                case "4":
+                    alert("Gracias por usar nuestro cajero. ¡Hasta Pronto!");
+                    continuar = false;
+                    break;
+                default:
+                    alert("Opcion inválida. Por favor, intente nuevamente");
+            }
+        }
+    } else {
+        alert("Ha excedido el número de intentos. Contactese con el banco ");
+    }
 }
+
+cajeroAutomatico();
